a55c6db4878e3e76f70f0d602e5d42d5bdc016bd
virogg <itisinter@gmail.com>
2026-05-19 15:02:26 +0300

    add lab11 images and README

--- lab11/solution/cmd/traceroute/main.go
+++ lab11/solution/cmd/traceroute/main.go
@@ -16,7 +16,7 @@ func main() {
 	queries := flag.Int("queries", 3, "number of probes per hop")
 	maxHops := flag.Int("max-hops", 30, "maximum TTL")
 	timeout := flag.Duration("timeout", 2*time.Second, "per-probe reply timeout")
-	resolve := flag.Bool("resolve", true, "reverse-DNS resolve hop names")
+	resolve := flag.Bool("resolve", false, "reverse-DNS resolve hop names")
 	verbose := flag.Bool("verbose", false, "log every sent/received packet to stderr")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s <host> [flags]\n", os.Args[0])
